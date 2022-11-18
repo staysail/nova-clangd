dceaee67255e0ab3ef3baed2343d8d5332bce596
Garrett D'Amore <garrett@damore.org>
2022-11-17 22:08:07 -0800

    Fixes for unions and field specifiers.

--- ClangD.novaextension/Queries/c-symbols.scm
+++ ClangD.novaextension/Queries/c-symbols.scm
@@ -47,11 +47,14 @@
 
 ((enum_specifier (enumerator_list (enumerator name:(identifier) @name) @subtree (#set! role enum-member))))
 
+((union_specifier name: (type_identifier) @name) @subtree (#set! role union))
+
 ((comment) @subtree (#set! role comment))
 
-((declaration declarator: (identifier) @name @subtree (#set! role variable)))
-((declaration (init_declarator declarator: (identifier) @name) @subtree (#set! role variable)))
+((((_) declarator: (identifier) @name) @subtree (#set! role variable)))
+((((_) declarator: (field_identifier) @name) @subtree (#set! role property)))
 
 ; preprocessor queries
 ((preproc_def name: (identifier) @name) @subtree (#set! role constant))
 ((preproc_function_def name: (identifier) @name) @subtree (#set! role function))
+
